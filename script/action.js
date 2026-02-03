9d44de250db59a31cccb9db54319d2fe6de1bc2e
shdls1016-max <shdls1016@gmail.com>
2026-02-03 18:00:08 +0900

    26.02.03 학원에서

---
+++ script/action.js
@@ -0,0 +1,33 @@
+/* html에서 script를 html최하단이 아닌 곳에 연결했을때 사용(ready()) */
+$(document).ready(function(){
+    
+/*     $(window).scroll(function(){
+        let scrT = $(window).scrollTop();
+        let part2T = $('.part2').offset().top;
+        
+
+        if(scrT >= part2T - 200){
+            $('.part2').addClass('on')
+        }
+        if(scrT >= part2T){
+            $('.part2 .sagak').css({
+                borderRadius:(scrT - part2T) * 0.3,
+                left:(scrT - part2T),
+                transform:'rotate(' + (scrT - part2T) + 'deg)'
+            })
+        } else{
+            $('.part2 .sagak').css({
+                borderRadius:0,
+                left:0,
+                transform:'rotate(0deg)'
+
+            })
+        }
+
+        
+    }) */
+       
+
+   
+})  
+
