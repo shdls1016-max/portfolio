cf5344fdcb0e2d7292d17185f87fb00383f43639
shdls1016-max <shdls1016@gmail.com>
2026-02-04 18:03:25 +0900

    0204 학원에서

--- script/action.js
+++ script/action.js
@@ -1,33 +1,32 @@
 /* html에서 script를 html최하단이 아닌 곳에 연결했을때 사용(ready()) */
 $(document).ready(function(){
     
-/*     $(window).scroll(function(){
-        let scrT = $(window).scrollTop();
-        let part2T = $('.part2').offset().top;
-        
+$('.mini').mouseover(function(){
+    $(this).addClass('on')
+})
+$('.mini').mouseout(function(){
+    $(this).removeClass('on')
+})
 
-        if(scrT >= part2T - 200){
-            $('.part2').addClass('on')
-        }
-        if(scrT >= part2T){
-            $('.part2 .sagak').css({
-                borderRadius:(scrT - part2T) * 0.3,
-                left:(scrT - part2T),
-                transform:'rotate(' + (scrT - part2T) + 'deg)'
-            })
-        } else{
-            $('.part2 .sagak').css({
-                borderRadius:0,
-                left:0,
-                transform:'rotate(0deg)'
+$('.mini').hover(function(){
+    if($(this).hasClass('dong')){
+        $('#webmain').css('background-image','url(/images/web1.jpg)');
+    }
+    if($(this).hasClass('book')){
+        $('#webmain').css('background-image','url(/images/web2.jpg)');
+    }
+    if($(this).hasClass('dl')){
+        $('#webmain').css('background-image','url(/images/web3.jpg)');
+    }
+    if($(this).hasClass('dive')){
+        $('#webmain').css('background-image','url(/images/web4.jpg)');
+    }
+},
+function(){   //마우스 아웃시 원래이미지로
+    $('#webmain').css('background-image','url(/images/web1.jpg)');
+}
+);
+});
 
-            })
-        }
 
-        
-    }) */
-       
-
-   
-})  
 
